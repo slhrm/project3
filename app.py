4206ce11dab1994bca1a010016077c155ce06afe
slhrm <srmumun24a@gmail.com>
2026-01-22 10:35:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Log in система")
 name = st.text_input("Въведи име")
 if st.button("Провери"):
-if name.strip()=="":
+  if name.strip()=="":
   st.warning("Моля въведи текст")
   elif not name.isalpha():
   st.warning(".......")
