d140f45c482ab058c47bde5729498883418f4bd8
slhrm <srmumun24a@gmail.com>
2026-01-22 10:31:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import 
+import streamlit as st
 
-
+st.title("Log in система")
 name = st.text_input("Въведи име")
 if st.button("Провери")
-if name
+if name.strip()=="":
+  st.warning("Моля въведи текст")
+elif not name.isalpha():
+st.warning(".......")
+else st.success("Текстът е въведен правилно")
+  
